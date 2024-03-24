db19f0b2ed030da993376f62a3a06cd9406f241c
arpangill90 <arpangill90@gmail.com>
2024-03-24 14:39:15 -0400

    Add a controller to start the job

---
+++ src/main/java/com/arpan/springbatch/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.arpan.springbatch.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeController {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @PostMapping
+    public void importCsvToDB() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis())
+                .toJobParameters();
+
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            log.error("Exception...", e);
+        }
+    }
+}
