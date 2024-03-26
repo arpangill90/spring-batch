22bd2ce50abccf90eb8cdfffba95ba6e78c3d7c9
arpangill90 <arpangill90@gmail.com>
2024-03-25 20:21:22 -0400

    Use ID auto generated values

--- src/main/java/com/arpan/springbatch/config/EmployeeProcessor.java
+++ src/main/java/com/arpan/springbatch/config/EmployeeProcessor.java
@@ -7,6 +7,7 @@ public class EmployeeProcessor implements ItemProcessor<Employee, Employee> {
     @Override
     public Employee process(Employee employee) throws Exception {
         //any transformation would go here, for now keeping it simple
+        employee.setId(null); // to make use of hibernate auto generated id
         return employee;
     }
 }
